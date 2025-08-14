ad5917c9ab91574c0c44bddf4e9415525a7142f0
Shantanu-Kashyap <shantanukumarsko@gmail.com>
2025-08-14 14:37:46 +0530

    Backend updated: CORS for AWS + localhost, constants, app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,24 @@ const connectDB = require("./config/database");
 const cookieParser = require('cookie-parser');
 const cors =require('cors');
 
+const allowedOrigins = [
+  "http://localhost:5173",   // local frontend
+  "http://13.53.90.173:5173" // AWS deployed frontend
+];
+
 app.use(cors({
-    origin: "http://13.53.90.173:5173",
-    credentials: true,
+  origin: function(origin, callback){
+    // allow requests with no origin (like Postman or server-to-server)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
