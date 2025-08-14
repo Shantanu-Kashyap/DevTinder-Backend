b91799df8abce230a51e32d3da97c89c90ac7ea3
Shantanu-Kashyap <shantanukumarsko@gmail.com>
2025-08-14 10:30:12 +0530

    Updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const connectDB = async () => {
-mongoose.connect("mongodb+srv://Shantanu167:Sko9431401185%40@sko.buogje3.mongodb.net/devTinder");
+mongoose.connect("mongodb+srv://Shantanu167:cHG5rFxU4hNym73D@sko.buogje3.mongodb.net/devTinder");
 }
 
 module.exports = connectDB;
